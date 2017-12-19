e56ff5562813660af1d1575d5966bfabffebdffe
gundamkeroro <1056262283fengxl@gmail.com>
2017-12-18 22:52:57 -0800

    remove sudo

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 apt update
-apt-get install nginx
-pip install flask gunicorn
+apt-get install -y nginx
+pip install -y flask gunicorn
 
 git clone https://github.com/gundamkeroro/nsfw_flaskAPI.git
 cd nsfw_flaskAPI
 cp open_nsfw /etc/nginx/sites-available/open_nsfw
-sudo ln -s /etc/nginx/sites-available/open_nsfw /etc/nginx/sites-enabled
-sudo nginx -t
-sudo service nginx restart
+ln -s /etc/nginx/sites-available/open_nsfw /etc/nginx/sites-enabled
+nginx -t
+service nginx restart
 
 gunicorn -w 8 -b 127.0.0.1:4000 NSFW:app
 
